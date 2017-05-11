e02460dcf5fe9558bd656adcd40d31bba757f690
eric guan <guanzo91@gmail.com>
2017-05-11 00:09:44 -0400

    succesfully switched intro to canvas, it looks much smoother

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,27 +1,16 @@
 <template>
     <div class="root">
-		<svg class="intro">
-			<g v-for="(slide,i) in slides" v-if="i <= index" >
-				<defs>
-					<linearGradient :id="'gradient'+i" y2="1" x2="0">
-						<stop offset=".1" :stop-color="slides[i].gradient[0]" />
-						<stop offset=".9" :stop-color="slides[i].gradient[1]" />
-					</linearGradient>
-					<mask :id="'mask'+i" class="masks">
-						<path :d="slide.d" :transform="slide.transform"></path>
-					</mask>
-				</defs>
-
-				<svg viewBox="0 0 1000 500" preserveAspectRatio="none">
-					<rect width="100%" height="100%" :mask="`url(#mask${i})`" :fill="`url(#gradient${i})`"/>
-				</svg>
-
-				<svg viewBox="0 0 1000 500" preserveAspectRatio="xMidYMid meet">
-					<text :style="slide.textStyle" class="glow":mask="`url(#mask${i})`" x="50%" y="50%"  alignment-baseline="central" v-html="slide.text"></text>
-				</svg>
-			</g>
-		</svg>
-	
+		<canvas ref="canvas"></canvas>
+		<a href="#about" class="scroll-down"  @click="onClick">
+			<svg v-visible="showUI" viewBox="0 0 100 100">
+				<g>
+					<path class="circle" :d="circlePath(50,50,45)"></path>
+					<path d="M30 35 L 50 50 L 70 35"></path>
+					<path d="M30 55 L 50 70 L 70 55"></path>
+				</g>
+			</svg>
+		</a>
+	<!--
         <svg class="global-defs">
             <defs>
                 <filter id="glow">
@@ -32,108 +21,164 @@
                     </feMerge>
                 </filter>
             </defs>
-        </svg>
-		<a href="#about" class="scroll-down"  @click="onClick">
-			<svg v-visible="showUI" viewBox="0 0 100 100">
-				<g>
-					<path class="circle" :d="circlePath(50,50,45)"></path>
-					<path d="M30 35 L 50 50 L 70 35"></path>
-					<path d="M30 55 L 50 70 L 70 55"></path>
-				</g>
-			</svg>
-		</a>
+        </svg>-->
     </div>
 </template>
 
 <script>
 import * as d3 from 'd3'
+import Vue from 'vue'
+import throttle from 'lodash/throttle'
 
 export default {
     name:'intro-section',
     data () {
-		//viewBox 0 0 1000 500 
-		//cubic-bezier(0.645, 0.045, 0.355, 1)
-		//above control point coordinates are for 1:1 x/y ratio. 
-		//my viewbox has ratio 2:1, so 0.045 = 0.0225, 1 = 0.5
-		//then multiplied by 1000 to scale to my viewbox 
-		var waveToRight = `M0 0
-							h1000
-							c645 22.5 355 500 1000 500
-							h-2000
-							Z`;
-		var waveToLeft = `M0 500
-							c645 -22.5 355 -500 1000 -500
-							h1000
-							v500
-							Z`;
         return {
             index: 0,
 			duration: 3000,
+			width:0,
+			height:0,
+			ctx:null,
+			interpolator:null,
+			startTime:0,
 			showUI: false,
 			slides:[
 				{
 					text:"Hi, my name is Eric",
 					gradient: ['#da7352','#d64759'],//red/orange
-					d: waveToRight,
-					transform:'translate(-2000,0)',
-					transformTo:'translate(0,0)'
 				},
 				{
 					text:"I'm a full stack developer",
 					gradient:['#38aecc','#2E3192'],
-					d: waveToLeft,
-					transform:'translate(1000,0)',
-					transformTo:'translate(-1000,0)'
 				},
 				{
 					text:'I create web applications',
 					gradient: ['#12B05B','#005A2A'],
-					d: waveToRight,
-					transform:'translate(-2000,0)',
-					transformTo:'translate(0,0)'
 				},
 				{
 					text:'Thanks for visiting',
 					gradient: ['#A3A1FF ','#3A3897'],//purple
-					d: waveToLeft,
-					transform:'translate(1000,0)',
-					transformTo:'translate(-1000,0)',
-					textStyle:{
-						'animation-name':'last-slide',
-					}
 				}
 			]
         }
     },
 	computed:{
-		currentSlide(){
-			return this.slides[this.index]
+		midX(){
+			return this.width/2
+		},
+		midY(){
+			return this.height/2
+		},
+		clipToRightStartOrigin(){
+			return 0 - this.width*2
+		},
+		xScale(){
+			return d3.scaleLinear().range([0,this.width])
+		},
+		yScale(){
+			return d3.scaleLinear().range([0,this.height])
 		}
 	},
     mounted(){
-		this.slideMask();
-		setTimeout(()=>this.next(), this.duration)
+		Vue.nextTick(()=>{
+			this.setCanvasSize();
+			var canvas = this.$refs.canvas
+			this.interpolator = d3.interpolateNumber(0,this.width)
+			this.ctx = canvas.getContext('2d')
+			this.startTime = new Date()
+
+			requestAnimationFrame(this.draw)
+		})
+
 		setTimeout(()=>this.reveal(), 2000)
+
+		window.addEventListener('resize',throttle(this.setCanvasSize,100))
     },
     methods:{
-        next(){
-            this.index++;
-			this.slideMask();
-            if(this.index + 1 < this.slides.length)
-                setTimeout(()=>this.next(), this.duration)
+        draw(){
+			var {ctx, index, startTime, interpolator,duration,width,height} = this;
+            var elapsed = new Date() - startTime;
+			var normalizedTime = Math.min(elapsed,duration)/duration
+			var easedTime = d3.easeCubicInOut(normalizedTime);
+			var interpolatedWidth = interpolator(easedTime)
+			
+			if(index == 0){
+				ctx.fillStyle='#373737'
+				ctx.fillRect(0,0,width,height)
+			}else
+				this.background(index-1)
+			
+			ctx.save();
+
+			if(index%2 == 0)
+				this.animateClipToRight(interpolatedWidth)
+			else
+				this.animateClipToLeft(interpolatedWidth)
+			this.background(index)
+			ctx.restore()
+			
+			if(elapsed < duration)
+				requestAnimationFrame(this.draw)
+			else if(index + 1 < this.slides.length){
+				this.index++;
+				this.startTime = new Date()
+				requestAnimationFrame(this.draw)
+			}
         },
-		slideMask(){
-			let slide = this.currentSlide;
-			var duration = this.duration;
-            var interpolator = d3.interpolateString(slide.transform, slide.transformTo)
-            var t = d3.timer((elapsed)=>{
-                var normalizedTime = Math.min(elapsed,duration)/duration
-                var easedTime = d3.easeCubicInOut(normalizedTime);
-                slide.transform = interpolator(easedTime);
-				
-                if (elapsed > duration) 
-                    t.stop();
-            });
+		animateClipToRight(interpolatedWidth){
+			var {ctx, clipToRightStartOrigin, width,height,xScale:x, yScale:y} = this;
+
+			var currentOrigin = clipToRightStartOrigin + interpolatedWidth*2
+			var rectTopRight = currentOrigin + width
+			var curveBotRight = rectTopRight + width
+			ctx.beginPath()
+			ctx.moveTo(currentOrigin,0)
+			ctx.lineTo(rectTopRight, 0)
+			//cubic-bezier(0.645, 0.045, 0.355, 1) cubicInOut
+			ctx.bezierCurveTo(rectTopRight + x(.645), y(0.045), 
+							  rectTopRight + x(0.355), y(1),
+							  curveBotRight,height);
+			ctx.lineTo(currentOrigin, height)
+			ctx.clip();
+		},
+		animateClipToLeft(interpolatedWidth){
+			var {ctx, clipToRightStartOrigin, width, height,xScale:x, yScale:y} = this;
+
+			var clipToLeftStartOrigin = width;
+			var currentOrigin = clipToLeftStartOrigin - interpolatedWidth*2
+			var rectTopLeft = currentOrigin + width
+  			var rectTopRight = rectTopLeft + width
+
+			ctx.beginPath()
+			ctx.moveTo(currentOrigin,height)
+  			ctx.bezierCurveTo(currentOrigin + x(.645), y(.955),//1 - 0.045
+								currentOrigin + x(0.355), y(0),//1 - 1
+								rectTopLeft,0);
+			ctx.lineTo(rectTopRight, 0)
+			ctx.lineTo(rectTopRight, height)
+			ctx.clip();
+		},
+		background(index){
+			var {ctx, width,height,midX,midY} = this;
+			var slide = this.slides[index]
+			var gradient = ctx.createLinearGradient(midX,0,midY,height)
+			gradient.addColorStop(.1, slide.gradient[0]);
+			gradient.addColorStop(.9, slide.gradient[1]);
+			
+			ctx.fillStyle = gradient;
+			ctx.fillRect(0, 0, width, height);
+			
+			ctx.textAlign="center"
+			ctx.fillStyle='white'
+			ctx.font="bold 60px Raleway";
+			ctx.shadowColor="white"
+			ctx.shadowBlur="10"
+			ctx.fillText(slide.text,midX,midY);
+		},
+		setCanvasSize(){
+			var canvas = this.$refs.canvas
+			this.width = canvas.width = window.innerWidth;
+			this.height = canvas.height = window.innerHeight
 		},
 		reveal(){
 			this.showUI = true;
@@ -174,10 +219,8 @@ function tweenDash() {
 @duration: 3s;
 
 .root{
-    height: 100vh;
 	font-family: 'Raleway', sans-serif;
 }
-
 
 .scroll-down {
 	position: absolute;
